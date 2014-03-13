01afb866ac647d5d7db77f98a7eeac4026a1c770
lehebith <leif_hedly_92@hotmail.com>
2014-03-13 16:30:28 +0100

    Added "Mine utvalg"

--- functions.php
+++ functions.php
@@ -300,8 +300,7 @@ function drawHeader($db){
 					<td>
 						<a href ="#aktiviteter" id="valg">Aktiviteter</a>
 						<a>|<a/>
-						<a href ="home.php" id="valg">Utvalg</a>
-					</td>';
+						<a href ="home.php" id="valg">Utvalg</a>';
 						drawLogoutBtn($db);
 						echo'
 				</tr>
@@ -319,11 +318,13 @@ function drawLogoutBtn($db){
 	}
 
 	if (isset($_SESSION['id'])){
-		echo '<td width="45%"><a>';
+		echo '<a>|<a/><a href ="#mineutvalg" id="valg">Mine Utvalg</a><form method="POST" action=""></td><td width="45%"><a>';
 		getUserSurnameName($db, $_SESSION['id']);
 		echo'</a><a>|<a/><a href ="session.php" id="valg"><div class="LogoutText">Logg ut</div></a></td>';
 	} else {
-		echo	'<form method="POST" action="">
+		echo	'
+				</td>
+
 					<td width="16%"><input type="text" name="loginbrukernavn" placeholder="Brukernavn" maxlength="45" class="input_banner"></td>
 					<td width="16%"><input type="password" name="loginpassord" placeholder="Passord" maxlength="30" class="input_banner"></td>
 					<td width="10%"><input type="submit" value="Logg inn" class="btn"></td>
