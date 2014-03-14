b1aff6fc5d76bdebf830b89d3441e51f332d75dc
Magnusri <magnus_rydjord@hotmail.com>
2014-03-14 12:58:25 +0100

    Mengdetekst fix

--- utvalg.php
+++ utvalg.php
@@ -18,17 +18,14 @@ if (isset($_GET['utvalg'])){
 	echo "<br/><br/>
 	<fieldset style='width: 800px; margin: auto;'>
 		<legend>$utvalgsnavn</legend>
-		<p>Suspendisse vitae felis orci. Cras eget enim ac risus dapibus sagittis quis pellentesque enim. Cras bibendum dapibus ligula, ut dapibus diam euismod in. In cursus vel tellus dapibus interdum. Quisque aliquam consectetur iaculis. Maecenas mollis convallis odio, ac ultricies urna auctor in. Fusce id erat nunc. Nam a vestibulum est, in posuere purus. Donec ac augue vel risus convallis facilisis in ac dolor. Morbi ut purus et nulla sagittis consectetur ut vel orci. Proin sodales aliquam mi, id faucibus turpis vehicula ut. Curabitur vestibulum tincidunt justo eget fermentum.</p>
+		<p>Suspendisse vitae felis orci. Cras eget enim ac risus dapibus sagittis quis pellentesque enim. Cras bibendum dapibus ligula, ut dapibus diam euismod in. In cursus vel tellus dapibus interdum.<br/> Quisque aliquam consectetur iaculis. Maecenas mollis convallis odio, ac ultricies urna auctor in. Fusce id erat nunc. Nam a vestibulum est, in posuere purus.<br/><br/> Donec ac augue vel risus convallis facilisis in ac dolor. Morbi ut purus et nulla sagittis consectetur ut vel orci. Proin sodales aliquam mi, id faucibus turpis vehicula ut. Curabitur vestibulum tincidunt justo eget fermentum.</p>
 	</fieldset>
 	<br />
 	<br />
 	<fieldset style='width: 800px; margin: auto;'>
 	</style>>
 		<legend>Arrangementer</legend>
-		<p>asasdfasdggfdsgsdfasdf</p><br />
-		<p>asasdfasdggfdsgsasdfasdfdfasdf</p><br />
-		<p>asasdfasdggfdsgsdfasdf</p><br />
-		<p>asasdfasdggfdsgsasdfasdfdfasdf</p>
+		<p>Her kommer det en liste over arrangementer dette utvalget arrangerer!</p><br/><br/>
 	</fieldset>
 	";
 	/*if (isset($_SESSION['id'])){
