0cbb0e72637779b5e57d52c6354fb1f99222dbaa
Magnusri <magnus_rydjord@hotmail.com>
2014-03-17 12:50:48 +0100

    fix style.css ikke main.css

--- utvalg.php
+++ utvalg.php
@@ -1,7 +1,7 @@
 <?php
 //REQUIREMENTS FOR ALL PAGES
 require 'functions.php';
-echo '<link rel="stylesheet" type="text/css" href="main.css" />';
+echo '<link rel="stylesheet" type="text/css" href="style.css" />';
 //REQUIREMENTS FOR ALL PAGES
 
 drawHeader($db);
@@ -21,7 +21,7 @@ if (isset($_GET['utvalg'])){
 	echo "<br/><br/>
 	<fieldset style='width: 800px; margin: auto;'>
 		<legend>$utvalgsnavn</legend><br/>
-		<p>Suspendisse vitae felis orci. Cras eget enim ac risus dapibus sagittis quis pellentesque enim. Cras bibendum dapibus ligula, ut dapibus diam euismod in. In cursus vel tellus dapibus interdum.<br/> Quisque aliquam consectetur iaculis. Maecenas mollis convallis odio, ac ultricies urna auctor in. Fusce id erat nunc. Nam a vestibulum est, in posuere purus.<br/><br/> Donec ac augue vel risus convallis facilisis in ac dolor. Morbi ut purus et nulla sagittis consectetur ut vel orci. Proin sodales aliquam mi, id faucibus turpis vehicula ut. Curabitur vestibulum tincidunt justo eget fermentum.</p>
+		<p></p>
 		<br/>
 	";
 
