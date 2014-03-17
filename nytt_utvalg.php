93dcc0e5881f6816bec46230d445e15981d5e080
lehebith <leif_hedly_92@hotmail.com>
2014-03-17 14:24:47 +0100

    styling nytt_utvalg

--- nytt_utvalg.php
+++ nytt_utvalg.php
@@ -15,19 +15,19 @@ echo '
 			<h1>Registrer nytt utvalg</h1>
 			<label>
 				<span>Navn</span>
-				<input type="text" class="input_text" name="navn" pattern="[A-Za-zØÆÅøæå]{1,20}" autofocus required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
+				<input type="text" class="input_text" name="navn" autofocus required="" maxlength="50" title="Mellom 1-50 tegn langt."/>
 			</label>
 			<label>
 				<span>Forkortelse av navn</span>
-				<input type="text" class="input_text" name="kortnavn" pattern="[A-Za-zØÆÅøæå]{1,20}" autofocus required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
+				<input type="text" class="input_text" name="kortnavn" maxlength="6" required="" title="Mellom 1-6 tegn langt."/>
 			</label>
 			<label>
 				<span>Kort beskrivelse</span>
-				<input type="text" class="input_text" name="kortbeskrivelse" pattern="[A-Za-zØÆÅøæå]{1,20}" autofocus required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
+				<input type="text" class="input_text" name="kortbeskrivelse" maxlength="80" required="" title="Mellom 1-80 tegn langt."/>
 			</label>
 			<label>
 				<span>Lang Beskrivelse</span>
-				<textarea rows="15" cols="200" name="langbeskrivelse" class="input_text" id="large_input_field"></textarea>
+				<textarea rows="15" cols="200" name="langbeskrivelse" required="" maxlength="1000" class="input_text" id="large_input_field" title="Mellom 1-80 tegn langt."></textarea>
 			</label>
 			<label align="center">
 				<input type="submit"  class="button" value="Opprett utvalg">
