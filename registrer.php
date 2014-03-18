0a1e6e5d98bf979c7984e45cd9b79e10fcec763d
lehebith <leif_hedly_92@hotmail.com>
2014-03-18 16:27:58 +0100

    Du kan nå ha mer enn 1 navn

--- registrer.php
+++ registrer.php
@@ -35,35 +35,35 @@ if (isset($_POST['fornavn']) && isset($_POST['etternavn']) && isset($_POST['stud
 	<fieldset>
 		<div class="pure-control-group">
 			<label for="name">Fornavn</label>
-			<input type="text" class="input_text" name="fornavn" pattern="[A-Za-zØÆÅøæå]{1,20}" autofocus required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
+			<input type="text" class="pure-input-1-3"  name="fornavn" pattern="[A-Za-zØÆÅøæå ]{1,20}" autofocus required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
 		</div>
 		<div class="pure-control-group">
 			<label for="name">Etternavn</label>
-			<input type="text" class="input_text" name="etternavn" pattern="[A-Za-zØÆÅøæå]{1,20}" required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
+			<input type="text"class="pure-input-1-3" name="etternavn" pattern="[A-Za-zØÆÅøæå ]{1,20}" required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
 		</div>
 		<div class="pure-control-group">
 			<label for="name">Studentnummer</label>
-			<input type="text" class="input_text" name="studentnummer" pattern="[0-9]{6}" required="" title="Seks siffer fra 0-9"/>
+			<input type="text" class="pure-input-1-3" name="studentnummer" pattern="[0-9]{6}" required="" title="Seks siffer fra 0-9"/>
 		</div>
 		<div class="pure-control-group">
 			<label for="name">Brukernavn</label>
-			<input type="text" class="input_text" name="brukernavn" pattern="[A-Za-z0-9ØÆÅøæå_-]{3,15}" required="" title="Mellom 3-15 langt, tilatte tegnsett er: a-Å, _ og -"/>
+			<input type="text" class="pure-input-1-3" name="brukernavn" pattern="[A-Za-z0-9ØÆÅøæå_-]{3,15}" required="" title="Mellom 3-15 langt, tilatte tegnsett er: a-Å, _ og -"/>
 		</div>
 		<div class="pure-control-group">
 			<label for="email">E-post</label>
-			<input type="email" class="input_text" name="email_1" maxlength="60" required=""/>
+			<input type="email" class="pure-input-1-3" name="email_1" maxlength="60" required=""/>
 		</div>
 		<div class="pure-control-group">
 			<label for="email">Gjenta E-post</label>
-			<input type="email" class="input_text" name="email_2" id="emailForm" id="email" autocomplete="off" maxlength="60" required=""/>
+			<input type="email" class="pure-input-1-3" name="email_2" id="emailForm" id="email" autocomplete="off" maxlength="60" required=""/>
 		</div>
 		<div class="pure-control-group">
 			<label for="password">Passord</label>
-			<input type="password" class="input_text" name="passord_1" pattern=".{5,20}" required="" title="Mellom 5 og 20 tegn"/>
+			<input type="password" class="pure-input-1-3" name="passord_1" pattern=".{5,20}" required="" title="Mellom 5 og 20 tegn"/>
 		</div>
 		<div class="pure-control-group">
 			<label for="password">Gjenta passord</label>
-			<input type="password" class="input_text" name="passord_2" id="passordForm" pattern=".{5,20}" required="" title="Mellom 5 og 20 tegn"/>
+			<input type="password" class="pure-input-1-3" name="passord_2" id="passordForm" pattern=".{5,20}" required="" title="Mellom 5 og 20 tegn"/>
 		</div  class="pure-control-group">
 		<div class="pure-control-group">
 			<label></label>
