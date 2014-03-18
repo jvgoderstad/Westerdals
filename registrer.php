a647d9746f2153d9f6df41fe4f02b7c789ce67ae
lehebith <leif_hedly_92@hotmail.com>
2014-03-18 16:08:06 +0100

    redesign of registrer.php

--- registrer.php
+++ registrer.php
@@ -4,14 +4,14 @@ echo '<link rel="stylesheet" type="text/css" href="styles.css">';
 echo '<script type="text/javascript" language="JavaScript">
 		function checkEmail(theForm) {
 			if (theForm.email_1.value != theForm.email_2.value) {
-		    	document.getElementById("emailForm").style.background = "red";
-		        return false;
-		    }
-		    if (theForm.passord_1.value != theForm.passord_2.value) {
-		    	document.getElementById("passordForm").style.background = "red";
-		        return false;
-		    }
-		    return true;
+				document.getElementById("emailForm").style.background = "red";
+				return false;
+			}
+			if (theForm.passord_1.value != theForm.passord_2.value) {
+				document.getElementById("passordForm").style.background = "red";
+				return false;
+			}
+			return true;
 		}
 	</script>';
 drawHeader($db);
@@ -19,54 +19,60 @@ drawHeader($db);
 if (isset($_POST['fornavn']) && isset($_POST['etternavn']) && isset($_POST['studentnummer']) && isset($_POST['brukernavn']) && isset($_POST['email_1']) && isset($_POST['email_2']) && isset($_POST['passord_1']) && isset($_POST['passord_2'])){
 	if (!empty($_POST['fornavn']) && !empty($_POST['etternavn']) && !empty($_POST['studentnummer']) && !empty($_POST['brukernavn']) && !empty($_POST['email_1']) && !empty($_POST['email_2']) && !empty($_POST['passord_1']) && !empty($_POST['passord_2'])){
 		addUser($db, $_POST['brukernavn'], $_POST['passord_1'], $_POST['fornavn'], $_POST['etternavn'], $_POST['email_1'], $_POST['studentnummer']);
-		
+
 		echo '
 			<div class="boxReg">
 				<h1>Du har blitt registrert! Fortsett til Alle utvalg!</h1>
 			</div>
 		';
-		
+
 	} else {
 		echo "Fyll inn alle feltene!";
 	}
 } else {
 	echo '
-		<form method="POST" action="registrer.php" onsubmit="return checkEmail(this);">
-		<div class="boxReg">
-			<h1>Registrer ny bruker</h1>
-			<label>
-				<span>Fornavn</span>
-				<input type="text" class="input_text" name="fornavn" pattern="[A-Za-zØÆÅøæå]{1,20}" autofocus required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
-			</label>
-			<label>
-				<span>Etternavn</span>
-				<input type="text" class="input_text" name="etternavn" pattern="[A-Za-zØÆÅøæå]{1,20}" required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
-			</label>
-			<label>
-				<span>Studentnr</span>
-				<input type="text" class="input_text" name="studentnummer" pattern="[0-9]{6}" required="" title="Seks siffer fra 0-9"/>
-			</label>
-			<label>
-				<span>Brukernavn</span>
-				<input type="text" class="input_text" name="brukernavn" pattern="[A-Za-z0-9ØÆÅøæå_-]{3,15}" required="" title="Mellom 3-15 langt, tilatte tegnsett er: a-Å, _ og -"/>
-			</label>
-			<label>
-				<span>E-post</span>
-				<input type="email" class="input_text" name="email_1" maxlength="60" required=""/>
-				<span>Gjenta</span>
-				<input type="email" class="input_text" name="email_2" id="emailForm" id="email" autocomplete="off" maxlength="60" required=""/>
-			</label>
-			<label>
-				<span>Passord</span>
-				<input type="password" class="input_text" name="passord_1" pattern=".{5,20}" required="" title="Mellom 5 og 20 tegn"/>
-				<span>Gjenta</span>
-				<input type="password" class="input_text" name="passord_2" id="passordForm" pattern=".{5,20}" required="" title="Mellom 5 og 20 tegn"/>
-			</label>
-			<label align="center">
-				<input type="submit"  class="button" value="Registrer">
-			</label>
+	<form method="POST" action="registrer.php" class="pure-form pure-form-aligned" onsubmit="return checkEmail(this);">
+	<fieldset>
+		<div class="pure-control-group">
+			<label for="name">Fornavn</label>
+			<input type="text" class="input_text" name="fornavn" pattern="[A-Za-zØÆÅøæå]{1,20}" autofocus required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
 		</div>
-	</form>
+		<div class="pure-control-group">
+			<label for="name">Etternavn</label>
+			<input type="text" class="input_text" name="etternavn" pattern="[A-Za-zØÆÅøæå]{1,20}" required="" title="Mellom 1-20 langt, tilatte tegnsett er: a-Å"/>
+		</div>
+		<div class="pure-control-group">
+			<label for="name">Studentnummer</label>
+			<input type="text" class="input_text" name="studentnummer" pattern="[0-9]{6}" required="" title="Seks siffer fra 0-9"/>
+		</div>
+		<div class="pure-control-group">
+			<label for="name">Brukernavn</label>
+			<input type="text" class="input_text" name="brukernavn" pattern="[A-Za-z0-9ØÆÅøæå_-]{3,15}" required="" title="Mellom 3-15 langt, tilatte tegnsett er: a-Å, _ og -"/>
+		</div>
+		<div class="pure-control-group">
+			<label for="email">E-post</label>
+			<input type="email" class="input_text" name="email_1" maxlength="60" required=""/>
+		</div>
+		<div class="pure-control-group">
+			<label for="email">Gjenta E-post</label>
+			<input type="email" class="input_text" name="email_2" id="emailForm" id="email" autocomplete="off" maxlength="60" required=""/>
+		</div>
+		<div class="pure-control-group">
+			<label for="password">Passord</label>
+			<input type="password" class="input_text" name="passord_1" pattern=".{5,20}" required="" title="Mellom 5 og 20 tegn"/>
+		</div>
+		<div class="pure-control-group">
+			<label for="password">Gjenta passord</label>
+			<input type="password" class="input_text" name="passord_2" id="passordForm" pattern=".{5,20}" required="" title="Mellom 5 og 20 tegn"/>
+		</div  class="pure-control-group">
+		<div class="pure-control-group">
+			<label></label>
+			<button type="submit" class="pure-button pure-button-primary">Registrer</button>
+		</div>
+	</fieldset>
+</form>
+
+
 	';
 }
 
