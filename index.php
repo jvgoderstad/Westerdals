55e0b496cae1648cc514e1adba4b34d91d991a99
jvgoderstad <jvgoderstad@gmail.com>
2014-03-18 12:37:01 +0100

    index v2

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 //REQUIREMENTS FOR ALL PAGES-->
 require 'functions.php';
+echo '<link rel="stylesheet" type="text/css" href="style.css" />';
+echo '<link rel="stylesheet" type="text/css" href="main.css" />';
 //REQUIREMENTS FOR ALL PAGES-->
 
 drawHeader($db);
 
 echo"
 <fieldset class='arrField'>
-		<legend class='arrLegend'>Arrangementer</legend><br/>
-		<p>Her kommer det en liste over arrangementer dette utvalget arrangerer!</p><br/><br/>
+		<legend class='arrLegend'>Startside</legend><br/>
+		<p>Her kan du  som student lese om, og registrere deg på ønskede utvalg på Westerdals.</p><br/><br/>
 	</fieldset>
 ";
 ?>
