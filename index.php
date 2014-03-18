324193f75a99de80da8dbc1d2ef8c919cb00bd75
jvgoderstad <jvgoderstad@gmail.com>
2014-03-18 12:42:55 +0100

    index_v3

--- index.php
+++ index.php
@@ -8,9 +8,13 @@ echo '<link rel="stylesheet" type="text/css" href="main.css" />';
 drawHeader($db);
 
 echo"
+
+</br>
+</br>
 <fieldset class='arrField'>
-		<legend class='arrLegend'>Startside</legend><br/>
-		<p>Her kan du  som student lese om, og registrere deg på ønskede utvalg på Westerdals.</p><br/><br/>
+		<legend class='arrLegend'>Velkommen til Westerdals utvalgs side!</legend><br/>
+		<p>Her kan du  som student lese om, og registrere deg på ønskede utvalg på Westerdals.
+		Klikk på Alle utvalg eller registrer deg for å begynne.</p><br/><br/>
 	</fieldset>
 ";
 ?>
