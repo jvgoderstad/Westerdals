7532a89e12ddcdb58ef3840b605eee4932d98ad8
jvgoderstad <jvgoderstad@gmail.com>
2014-03-18 12:33:08 +0100

    index

--- index.php
+++ index.php
@@ -4,3 +4,11 @@ require 'functions.php';
 //REQUIREMENTS FOR ALL PAGES-->
 
 drawHeader($db);
+
+echo"
+<fieldset class='arrField'>
+		<legend class='arrLegend'>Arrangementer</legend><br/>
+		<p>Her kommer det en liste over arrangementer dette utvalget arrangerer!</p><br/><br/>
+	</fieldset>
+";
+?>
