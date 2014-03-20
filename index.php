bb7cd60be692bd657fde5d668588f5de30be95f4
Magnusri <magnus_rydjord@hotmail.com>
2014-03-20 10:52:56 +0100

    index redirect til home.php

--- index.php
+++ index.php
@@ -7,14 +7,5 @@ echo '<link rel="stylesheet" type="text/css" href="main.css" />';
 
 drawHeader($db);
 
-echo"
-
-</br>
-</br>
-<fieldset class='arrField'>
-		<legend class='arrLegend'>Velkommen til Westerdals utvalgs side!</legend><br/>
-		<p>Her kan du  som student lese om, og registrere deg på ønskede utvalg på Westerdals.
-		Klikk på Alle utvalg eller registrer deg for å begynne.</p><br/><br/>
-	</fieldset>
-";
+header('Location: home.php');
 ?>
